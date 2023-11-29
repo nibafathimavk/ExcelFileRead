e3fdf932d04b91a358f0a40e467ffd2d8942f965
ASUS <ASUS@LAPTOP-QGMSOUBB>
2023-11-29 17:56:24 +0300

    Initial Commit of ExcelFileRead program

---
+++ ExcelRead/src/main/java/ExcelPkg/Excel.java
@@ -0,0 +1,90 @@
+package ExcelPkg;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Excel {
+	public static XSSFWorkbook w;
+
+
+
+	public static XSSFSheet s;
+
+
+
+	public static FileInputStream f;
+
+
+
+	public static String readStringData(int i,int j) throws IOException {
+
+
+
+	f= new FileInputStream("C:\\Users\\ASUS\\eclipse-workspace\\ExcelRead\\src\\main\\resources\\Student.xlsx");
+
+
+
+	w= new XSSFWorkbook(f);
+
+
+
+	s= w.getSheet("Sheet1");
+
+	Row r=s.getRow(i);
+
+
+
+	Cell c=r.getCell(j);
+
+
+
+	return c.getStringCellValue();
+
+
+
+	}
+
+
+
+	public static String readIntegerData(int i,int j) throws IOException {
+
+		
+
+
+
+			f= new FileInputStream("C:\\Users\\ASUS\\eclipse-workspace\\ExcelRead\\src\\main\\resources\\Student.xlsx");
+
+
+
+			w= new XSSFWorkbook(f);
+
+
+
+			s= w.getSheet("Sheet1");
+
+
+
+			Row r=s.getRow(i);
+
+
+
+			Cell c=r.getCell(j);
+
+
+
+			int value=(int) c.getNumericCellValue();
+
+
+
+			return String.valueOf(value);
+
+
+
+			}
+
+	
+}
---
+++ ExcelRead/src/main/java/ExcelPkg/ExcelMain.java
@@ -0,0 +1,24 @@
+package ExcelPkg;
+
+import java.io.IOException;
+
+public class ExcelMain {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String name =Excel.readStringData(2,0);
+
+
+
+		System.out.println("Name :"+ "\t"+name);
+
+
+
+		String id= Excel.readIntegerData(2, 1);
+
+
+
+		System.out.println("id :"+"\t"+ id);
+	}
+
+}
